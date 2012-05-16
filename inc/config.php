4a3b595a4648e9fc6866550d8d1e528d0e7ca860
Alex Kovalevych <alexkovalevych@gmail.com>
2012-05-16 14:06:20 +0300

    set application specific container classes

--- inc/config.php
+++ inc/config.php
@@ -12,8 +12,8 @@ if (!isset($content_width)) { $content_width = 940; }
 define('POST_EXCERPT_LENGTH',       40);
 define('WRAP_CLASSES',              'container');
 define('CONTAINER_CLASSES',         'row');
-define('MAIN_CLASSES',              'span8');
-define('SIDEBAR_CLASSES',           'span4');
+define('MAIN_CLASSES',              'span10');
+define('SIDEBAR_CLASSES',           'span2');
 define('FULLWIDTH_CLASSES',         'span12');
 define('GOOGLE_ANALYTICS_ID',       '');
 
